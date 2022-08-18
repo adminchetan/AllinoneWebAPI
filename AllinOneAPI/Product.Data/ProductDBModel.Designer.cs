a098e60b5c286afebce4113ef148db53bb8bf495
neeraj joshi <neerajjoshi1000@gmail.com>
2022-08-18 22:16:05 +0530

    i have chaned nam of aaolicatinto laptop 2 name neeraj joshi

--- AllinOneAPI/Product.Data/ProductDBModel.Designer.cs
+++ AllinOneAPI/Product.Data/ProductDBModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\_GitHub\AllinoneWebAPI\AllinOneAPI\Product.Data\ProductDBModel.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\Git_hub\AllinoneWebAPI\AllinOneAPI\Product.Data\ProductDBModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
